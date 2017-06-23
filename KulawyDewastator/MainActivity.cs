dc5b459858843e716f7f08bbd50bfb9b5b108ff5
Wojciech Regulski <wojtekreg@gmail.com>
2017-06-23 20:33:49 +0200

    not bad

--- KulawyDewastator/MainActivity.cs
+++ KulawyDewastator/MainActivity.cs
@@ -1,18 +1,77 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
+using Android.Content;
 using Android.Widget;
 using Android.OS;
+using Android.Preferences;
+using Android.Telephony;
 
 namespace KulawyDewastator
 {
     [Activity(Label = "KulawyDewastator", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private EditTextPreference phoneTextPreference = new EditTextPreference(Application.Context);
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+
+            Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+            Button smsButton = FindViewById<Button>(Resource.Id.SendSms);
+            Button mailButton = FindViewById<Button>(Resource.Id.SendEmail);
+            EditText phoneNumber = FindViewById<EditText>(Resource.Id.PhoneNumber);
+            EditText messageText = FindViewById<EditText>(Resource.Id.MessageText);
+            EditText smsCount = FindViewById<EditText>(Resource.Id.SmsCount);
+            EditText messageTopic = FindViewById<EditText>(Resource.Id.MessegeTopic);
+            EditText toWhoMail = FindViewById<EditText>(Resource.Id.ToWhoMail);
+
+            //var cos = Resources.GetString(Resource.String.PhoneNumber);
+            //cos = "hgjds";
+            //phoneTextPreference.Text = "609941530";
+            phoneNumber.Text = phoneTextPreference.Text;
+
+            callButton.Click += (object sender, EventArgs e) =>
+            {
+                var callIntent = new Intent(Intent.ActionCall);
+                callIntent.SetData(Android.Net.Uri.Parse("tel:" + phoneNumber.Text));
+                StartActivity(callIntent);
+            };
+
+            smsButton.Click += (object sender, EventArgs e) =>
+            {
+                for (int i = 0; i < int.Parse(smsCount.Text); i++)
+                {
+                    /*var smsUri = Android.Net.Uri.Parse("smsto:"+phoneNumber.Text);
+                    var smsIntent = new Intent(Intent.ActionSendto, smsUri);
+                    smsIntent.PutExtra(messageText.Text,1);
+                    StartActivity(smsIntent);*/
+                    SmsManager.Default.SendTextMessage(phoneNumber.Text, null, messageText.Text, null, null);
+                }
+            };
+
+            mailButton.Click += (object sender, EventArgs e) =>
+            {
+                var email = new Intent(Intent.ActionSend);
+                email.PutExtra(Intent.ExtraEmail, new string[] { toWhoMail.Text });
+
+                email.PutExtra(Intent.ExtraSubject, messageTopic.Text);
+
+                email.PutExtra(Intent.ExtraText, messageText.Text);
+
+                email.SetType("message/rfc822");
+
+                StartActivity(email);
+            };
+        }
+
+        protected override void OnStop()
+        {
+            EditText phoneNumber = FindViewById<EditText>(Resource.Id.PhoneNumber);
+            phoneTextPreference.Text = phoneNumber.Text;
         }
     }
 }
--- KulawyDewastator/Resources/Resource.Designer.cs
+++ KulawyDewastator/Resources/Resource.Designer.cs
@@ -57,6 +57,58 @@ namespace KulawyDewastator
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f05000a
+			public const int CallButton = 2131034122;
+			
+			// aapt resource value: 0x7f050007
+			public const int MessageText = 2131034119;
+			
+			// aapt resource value: 0x7f050005
+			public const int MessegeTopic = 2131034117;
+			
+			// aapt resource value: 0x7f050003
+			public const int PhoneNumber = 2131034115;
+			
+			// aapt resource value: 0x7f05000c
+			public const int SendEmail = 2131034124;
+			
+			// aapt resource value: 0x7f05000b
+			public const int SendSms = 2131034123;
+			
+			// aapt resource value: 0x7f050009
+			public const int SmsCount = 2131034121;
+			
+			// aapt resource value: 0x7f050001
+			public const int ToWhoMail = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView1 = 2131034114;
+			
+			// aapt resource value: 0x7f050006
+			public const int textView2 = 2131034118;
+			
+			// aapt resource value: 0x7f050004
+			public const int textView3 = 2131034116;
+			
+			// aapt resource value: 0x7f050008
+			public const int textView4 = 2131034120;
+			
+			// aapt resource value: 0x7f050000
+			public const int textView5 = 2131034112;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
@@ -81,6 +133,9 @@ namespace KulawyDewastator
 			
 			// aapt resource value: 0x7f040000
 			public const int Hello = 2130968576;
+			
+			// aapt resource value: 0x7f040002
+			public const int PhoneNumber = 2130968578;
 			
 			static String()
 			{
